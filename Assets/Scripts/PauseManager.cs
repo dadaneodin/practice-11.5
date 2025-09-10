06915eeba313e1dff32a0f8cc3c75136004fea7a
dada <nemcevdaniil.5@gmail.com>
2025-09-10 19:46:27 +0300

    made practoice 11.5

---
+++ Assets/Scripts/PauseManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+    public GameObject pausePanel;
+    private bool isPaused = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePause();
+        }
+    }
+
+    public void TogglePause()
+    {
+        isPaused = !isPaused;
+        pausePanel.SetActive(isPaused);
+        Time.timeScale = isPaused ? 0 : 1;
+    }
+}
